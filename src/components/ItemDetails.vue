0fae9231070879813d873b40d97a37e67660acd3
paul <paulnyoiken@gmail.com>
2025-02-27 00:29:20 +0300

    :sparkles: feat: Add ItemDetails.vue to components and views folder

---
+++ src/components/ItemDetails.vue
@@ -0,0 +1,46 @@
+// src/components/ItemDetails.js
+<template>
+  <div class="item-details">
+    <h1>Item Details</h1>
+    <p v-if="item">Name: {{ item.name }}</p>
+    <p v-if="item">Category ID: {{ item.category_id }}</p>
+    <p v-if="item">Description: {{ item.description }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ItemDetails',
+  data() {
+    return {
+      item: null
+    }
+  },
+  mounted() {
+    axios.get(`https://wardrobe-management-o2bd.onrender.com/clothing-items/${this.$route.params.id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    .then(response => {
+      this.item = response.data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+}
+</script>
+
+<style scoped>
+.item-details {
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+.item-details p {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/ItemDetails.vue
@@ -0,0 +1,20 @@
+// src/views/ItemDetails.vue
+<template>
+  <div>
+    <Header />
+    <ItemDetails />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import ItemDetails from '../components/ItemDetails.vue';
+
+export default {
+  name: 'ItemDetailsView',
+  components: {
+    Header,
+    ItemDetails
+  }
+}
+</script>
