d0dfc8070d46df924651841bc2df97a71f1c3dd4
paul <paulnyoiken@gmail.com>
2025-02-27 00:14:56 +0300

    :sparkles: feat: Add Home.vue to components and views folder

---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <h1>Clothing Items</h1>
+    <ul class="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }} ({{ item.category_id }})
+        <router-link :to="{ name: 'itemDetails', params: { id: item.id } }">View Details</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    axios.get('https://wardrobe-management-o2bd.onrender.com/clothing-items', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    .then(response => {
+      this.items = response.data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+.item-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.item-list li {
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+.item-list li:last-child {
+  border-bottom: none;
+}
+
+.item-list a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+
+.item-list a:hover {
+  color: #3e8e41;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+// src/views/Home.vue
+<template>
+  <div>
+    <Header />
+    <Home />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Home from '../components/Home.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    Header,
+    Home
+  }
+}
+</script>
