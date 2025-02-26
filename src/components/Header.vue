106574ce08a23151baa663869ab5c62bd0de15ac
paul <paulnyoiken@gmail.com>
2025-02-27 00:23:10 +0300

    :sparkles: feat: Update Header.vue to components folder

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,43 +1,69 @@
+// src/components/Header.js
 <template>
-    <nav class="header">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/login">Login</router-link></li>
-        <li><router-link to="/register">Register</router-link></li>
-        <li><router-link to="/new-item">New Item</router-link></li>
-      </ul>
-    </nav>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  .header {
-    background-color: #333;
-    color: white;
-    padding: 1em;
-    text-align: center;
+  <nav class="header">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/new-item">Add New Item</router-link></li>
+      <li v-if="isLoggedIn"><button @click="logout">Logout</button></li>
+      <li v-else>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/register">Register</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  computed: {
+    isLoggedIn() {
+      return localStorage.getItem('token') !== null;
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/login');
+    }
   }
-  
-  .header ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-  
-  .header li {
-    display: inline-block;
-    margin-right: 20px;
-  }
-  
-  .header a {
-    color: white;
-    text-decoration: none;
-  }
-  
-  .header a:hover {
-    color: #ccc;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+
+.header ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.header li {
+  display: inline;
+  margin-right: 20px;
+}
+
+.header a {
+  color: white;
+  text-decoration: none;
+}
+
+.header button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.header button:hover {
+  background-color: #3e8e41;
+}
+</style>
